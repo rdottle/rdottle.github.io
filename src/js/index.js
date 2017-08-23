b5c0fe903af64c4b3ed118a22f85cea13a280fb0
Rachael Dottle <rachaelcdottle@gmail.com>
2017-08-23 12:16:41 -0400

    Add img, change

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,23 @@
-
-
-// function createCircle(){
-
-// var svgContainer = d3.select("header").append("svg")
-//                                     .attr("width", 200)
-//                                      .attr("height", 200);
- 
-//  //Draw the Circle
-//  var circle = svgContainer.append("circle")
-//                           .attr("cx", 20)
-//                           .attr("cy", 20)
-//                          .attr("r", 10);
-
-// console.log("test");
-// return circle;
-// }
-
-// createCircle();
-
-
-
 // create data
-var data = [];
-for (var i=0; i < 108; i++) {
- data.push(i);
-}
+var articles = 10;
+var data = articles;
+
 
 // Scale for radius
-var xr = d3.scale
-        .linear()
-        .domain([0, 108])
-        .range([0, 27]);
+var xr = 100;
 
 // Scale for random position
+var windowWidth = $(window).width();
+
 var randomPosition = function(d) {
-	var windowWidth = $(window).width();
+    var windowWidth = $(window).width();
     return Math.random() * windowWidth;
 }
 
 
-var tcColours = ['#daafd6', '#fffff', '#FCDDBC', '#D9DBBC', '#B8D8BA', '#797B84'];
+
+
+var tcColours = ['#f24a6b', '#fff101', '#1d2588'];
 var randomTcColour = function() {
   return Math.floor(Math.random() * tcColours.length);
 };
@@ -51,74 +29,61 @@ var randomTcColour = function() {
 	//d3.selectAll("article").select("h1").html()
 
 
-// CHANGING COLOR OF THE "Instructor Notes"
 
-//txt.attr("font-color","white")
-//txt.attr("color","red")
-
-// SVG viewport
-var svg = d3.select('header')
-  .append('svg')
-    .attr('width', '100%')
-    .attr('height', '40vh');
-
-var update = function() {
-	 var txt=d3.selectAll("article").select("h1")
-    	.style('color', tcColours[randomTcColour()]);
-    //var baseCircle = svg.selectAll('circle');
-    var baseRect = svg.selectAll('rect');
-
-    // Bind data
-    //baseCircle = baseCircle.data(data);
-    baseRect = baseRect.data(data);
-
-    // set the rects
-    baseRect.transition()
-             .duration(200)
-             .attr('width', xr)
-             .attr('height', xr)
-             .attr('x', randomPosition)
-             .attr('y', randomPosition)
-             .style('fill', tcColours[randomTcColour()]);
-
-    baseRect.enter()
-             .append('rect')
-             .attr('width', xr)
-             .attr('height', xr)
-             .attr('x', randomPosition)
-             .attr('y', randomPosition)
-             .style('fill', tcColours[randomTcColour()]);
+// var svg = d3.select("#circles")
+//     .append('svg')
+//     .attr('width', windowWidth)
+//     .attr('height', '20vh');
 
 
-    // set the circles
- /*   //baseCircle.transition()
-            .duration(250)
-            .attr('r', xr)
-            .attr('cx', randomPosition)
-            .attr('cy', randomPosition)
-            .attr('fill', "none")
-            .attr("stroke-width", 4)
-            .style('stroke', tcColours[randomTcColour()])
-             .style('fill', tcColours[randomTcColour()]);
+// //var update = function(){
 
- 
-    baseCircle.enter()
-            .append('circle')
-            .attr('r', xr)
-            .attr('cx', randomPosition)
-            .attr('cy', randomPosition)
-            .attr('fill', "none")
-            .attr("stroke-width", 4)
-            .style('stroke', tcColours[randomTcColour()])
-            .style('fill', tcColours[randomTcColour()]);*/
+// svg.selectAll('circle')
+//     .data(data)
+//     .enter()
+//     .append('circle')
+//     .attr('width', xr)
+//     .attr('r', xr)
+//     .attr('cx', randomPosition())
+//     .attr('cy', 100)
+//     .style('fill', tcColours[randomTcColour()]);
 
+
+ var dataset = [],
+    i = 0;
+
+    for(i=0; i<5; i++){
+        dataset.push(Math.round(Math.random()*100));
+    }        
+
+
+var update = function(){
+
+var randomheight = function(d) {
+
+var windowheight = $('svg.test').height();
+    return (windowheight * Math.random());
+}
+    var sampleSVG = d3.select("#circles")
+        .append("svg")
+        .attr("class", "test")
+        .attr("width", windowWidth)
+        .attr("height", '40vh');    
+
+    sampleSVG.selectAll("circle")
+        .data(dataset)
+        .enter().append("circle")
+        .style("stroke", "gray")
+        .style("fill", "black")
+        .attr('r', 40)
+        .attr("cx", randomPosition)
+        .attr("cy", randomheight);
+
+
+console.log(randomheight);
 
 }
 
 setInterval(function() {
     update();
 }, 1000);
-
-
-
-
